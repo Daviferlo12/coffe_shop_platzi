cf8b27aea6d69f1606fbe44dc370ac0e06bddc99
David Fernando López Barragán <dlopez@excellentiam.co>
2024-11-15 11:15:50 -0500

    Setting List products view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+# MODELS
+from products.models import Product
 
-# Create your views here.
+class ProductListView(TemplateView):
+    template_name = "products/products.html"
+    
+    def get_context_data(self):
+        products_list = Product.objects.all()
+        return {
+            "products_list" : products_list
+        }
