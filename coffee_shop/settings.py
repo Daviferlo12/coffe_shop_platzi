1656a832398470ab015522a234c4f99033e842f3
David Fernando López Barragán <dlopez@excellentiam.co>
2024-11-27 17:02:00 -0500

    Setting Products and Users App

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_tailwind',
     'products',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +126,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CRISPY CONFIGURATION
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+CRISPY_TEMPLATE_PACK = "tailwind"
+
+#  LOGIN  CONFIGURATION
+LOGIN_REDIRECT_URL = "all_products"
--- products/models.py
+++ products/models.py
@@ -7,6 +7,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="precio")
     available = models.BooleanField(default=True, verbose_name="disponible")
     photo = models.ImageField(upload_to="logos", null=True, blank=True, verbose_name="foto")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="fechaCreación")
 
     def __str__(self):
         return self.name
